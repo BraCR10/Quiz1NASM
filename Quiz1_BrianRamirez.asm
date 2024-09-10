3e634e64f5da638015ec0b8cf4dffd2a0ee949b2
Brian Ramirez <Brianramirez01arias@gmail.com>
2024-09-10 11:21:54 -0600

    cambios

--- Quiz1_BrianRamirez.asm
+++ Quiz1_BrianRamirez.asm
@@ -1,7 +1,7 @@
 ;   Programa que pasa una secuencia de bits a base 10   Quiz1_BrianRamirez.ASM
 ;   Objetive: Aprender a usar comandos de assembly y aprender a usar 
 ;   el macro io.mac
-;   inputs: Secuencia de unos y ceros(Menor a 80 digitos)
+;   inputs: Secuencia de unos y ceros(Menor a 32 digitos)
 ;   outputs: Numero decimal
 %include "io.mac"
 
@@ -13,36 +13,21 @@ nonbynary_error_msg   db  'El numero binario no es valido',0
 error_buffer_msg   db  'Error de buffer: El numero binario es demaciado largo(Mayor a 8bytes)',0
 author_info_msg   db  'Estudiante: Brian Ramirez Arias Carnet: 2024109557 ',0
 
-;confirm_msg1 db  'Repetir mensaje: ',0
-;confirm_msg2 db   ' veces? (S/N) ',0
-;welcome_msg db     'Bienvenido al el lenguaje Ensamblador ',0
-
 .UDATA
-;El programa esta echo para almacenar hasta una cantidad de 80bits
-bynarySecuence resb  10; buffer de 10bytes = 80bits
+;El programa esta echo para almacenar hasta una cantidad de 32bits
+bynarySecuence resb  4; buffer de 4bytes = 32bits
 
 .CODE
     .STARTUP
     PutStr welcome_msg
     nwln
     PutStr instruction_msg
-    GetStr bynarySecuence,10
-checkSecuence:
-    
+    GetStr bynarySecuence,4
+    mov edx,bynarySecuence  ;Almacena el dato en el registro edx
    
+checkSecuence:
+
     
     .EXIT
-;ask_count:
-;    PutStr welcome_msg
-;    GetInt CX
-;    PutStr welcome_msg
-;    PutInt CX
-;    PutStr welcome_msg
-   
-;display_msg:
-;    PutStr welcome_msg
-;    PutStr welcome_msg
-;    nwln
-;    loop display_msg
-;    .EXIT
+
 
